3e3be1c09e82233fd5e4993ecd022fa884f4c3ca
Jason Morrison <jmorrison@thoughtbot.com>
2011-01-19 15:58:08 -0500

    here is some stuff committed, what is it, who knows

--- app.rb
+++ app.rb
@@ -4,15 +4,12 @@ get '/' do
   erb :index
 end
 
-post '/' do
-  term = params[:term]
-
-
+get '/search' do
   username = ENV['CF_USERNAME']
   password = ENV['CF_PASSWORD']
-  term     = term
+  @term = params[:term]
 
-  cfwhat = CfWhat.new(username, password, term)
+  cfwhat = CfWhat.new(username, password, @term)
   cfwhat.search
   @results = cfwhat.results
 
